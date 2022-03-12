5659c974c929bceb0ec994fe40cf7ec689f23622
rizalblog <rizaldimohammad2002@gmail.com>
2022-03-12 20:22:01 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/coba', function () {
+    return view('coba');
+});
+
+Route::get('/admin', function () {
+    return 'Hello admin';
+});
