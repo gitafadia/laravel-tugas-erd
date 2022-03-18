a45aceb46738d26700717da1f2000bba1d9c1fb4
gitafadia <e41200971@polije.ac.id>
2022-03-18 09:13:36 +0700

    template

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -8,17 +8,17 @@
 
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
 
-<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css'}})">
 
-<link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+<link rel="stylesheet" href="{{ asset('bower_components/Ionicons/css/ionicons.min.css'}})">
 
-<link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+<link rel="stylesheet" href="{{ asset('bower_components/jvectormap/jquery-jvectormap.css"}})>
 
-<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+<link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css'}})">
 
-<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+<link rel="stylesheet" href="{{( asset('dist/css/skins/_all-skins.min.css'}})">
 
 
 <!--[if lt IE 9]>
